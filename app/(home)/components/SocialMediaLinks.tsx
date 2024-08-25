bc124ed0f898d728ab1be35d955ae446c60d1975
Belzzubair <107881300+abuakr-alzubir@users.noreply.github.com>
2024-08-25 14:44:53 +0300

    second commit

--- app/(home)/components/SocialMediaLinks.tsx
+++ app/(home)/components/SocialMediaLinks.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-const SocialMediaLinks = ({src, href, hasBackground=false}:{src:string, href?: string, hasBackground: boolean}) => {
+const SocialMediaLinks = ({src, href, hasBackground=false}:{src:string, href?: string, hasBackground?: boolean}) => {
   return (
     <a href={href} target='_blank' className={hasBackground ? 'bg-green-500 p-2 rounded-lg' : ''}>
         <Image src={`/${src}.png`} alt={src} width={24} height={24} />
